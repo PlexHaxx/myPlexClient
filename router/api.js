1b6470e400f2d7b562b15a0685367c338de9922a
Antonio Gil <agil@openbox.casa.local>
2015-12-07 17:14:27 +0100

    add api.js

--- router/api.js
+++ router/api.js
@@ -1,9 +1,53 @@
 var express = require('express');
-var plexApi = require('plex-api');
+var PlexApi = require('plex-api'),
+  plexApi = new PlexApi({
+    hostname: '192.168.0.100',
+    options: {
+      product: 'myPlexClient',
+      version: '0.1.0',
+    }
+  });
+
 var router = express.Router();
 
-router.get('/find', function(req, res, next) {
-  res.send('find');
+router.get('/cogerImagen', function(req, res, next) {
+  console.info(req.query.uri);
+  res.json('OK');
+});
+
+router.get('/status', function(req, res, next) {
+  plexApi.find('/status/sessions')
+    .then(function(data) {
+      res.json(data);
+    })
 })
 
-module.export = router;
+router.get('/list', function(req, res, next) {
+  plexApi.find(req.query.uri)
+    .then(function(data) {
+      res.json(data);
+    }, function(err) {
+      throw new Error('Not connect with Plex Server');
+    });
+})
+
+router.get('/find/:search', function(req, res, next) {
+  plexApi.find('/library/sections')
+    .then(function(data) {
+      res.json(data);
+    }, function(err) {
+      throw new Error('Not connect with Plex Server');
+    });
+});
+
+router.get('/', function(req, res, next) {
+  plexApi.find('/')
+    .then(function(data) {
+      res.send('OK');
+    }, function(err) {
+      res.send('KO');
+      throw new Error('Not connect with Plex Server');
+    })
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,5 @@
 var express = require('express');
 var path = require('path');
-var PlexApi = require('plex-api'),
-  plexApi = new PlexApi({
-    hostname: '192.168.0.100',
-    options: {
-      product: 'myPlexClient',
-      version: '0.1.0',
-    }
-  });
 
 var app = express();
 
@@ -17,51 +9,7 @@ app.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.get('/api/cogerImagen', function(req, res, next) {
-  console.info(req.query.uri);
-  res.json('OK');
-  // plexApi.find(req.query.uri)
-  //   .then(function(data) {
-  //     res.json(data);
-  //   }, function(err) {
-  //     throw new Error('Not connect with Plex Server');
-  //   });
-});
-
-app.get('/api/status', function(req, res, next) {
-  plexApi.find('/status/sessions')
-    .then(function(data){
-      res.json(data);
-    })
-})
-
-app.get('/api/list', function(req, res, next) {
-  plexApi.find(req.query.uri)
-    .then(function(data) {
-      res.json(data);
-    }, function(err) {
-      throw new Error('Not connect with Plex Server');
-    });
-})
-
-app.get('/api/find/:search', function(req, res, next) {
-  plexApi.find('/library/sections')
-    .then(function(data) {
-      res.json(data);
-    }, function(err) {
-      throw new Error('Not connect with Plex Server');
-    });
-});
-
-app.get('/api/', function(req, res, next) {
-  plexApi.find('/')
-    .then(function(data) {
-      res.send('OK');
-    }, function(err) {
-      res.send('KO');
-      throw new Error('Not connect with Plex Server');
-    })
-})
+app.use('/api', require('./router/api'));
 
 app.listen(3000);
 
